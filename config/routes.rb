3a837ccb13169bc723e1a3c48a32d46122addb7e
zhangqian <zhq9877@gmail.com>
2017-03-10 17:18:47 +0800

    step7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
   namespace :admin do
     resources :jobs do
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+
 end
